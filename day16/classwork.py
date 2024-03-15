91aa817cbdd38ea5b4605311ebece888fd3ac412
tazoabesadze <tazo.abesadze4@gmail.com>
2024-03-15 05:24:35 -0700

    day16

---
+++ day16/classwork.py
@@ -0,0 +1,109 @@
+#     დავიმახსოვღოთ:
+#  def - შექმნა, განსაზღვრა
+
+# def say_hi(user_name):
+#     print(user_name + " gamarjoba")
+
+
+# say_hi("nika")
+# say_hi("luka")
+# say_hi("gabrieli")
+# say_hi("dato")
+
+#შექმენით ფუნქცია, რომელიც მომხმარებელს შეეკითხება რიცხვს. ხოლო დაუპრინტავს ამ რიცხვზე 3ჯერ დიდ რიცხვს. 
+
+#გამოიძახეთ ეს ფუნქცია 5ჯერ, სხვადასხვა რიცხვებისთვის და დატესტეთ     --->
+
+
+       #"num" არის პარამეტრი (როცა ფუნქციას ვქმნით)
+# def num(num): 
+#     print(num * 3)
+
+    # "4, 11, ..., 5" არის არგუმრნტი (როცა ფუნქციას გამოიძახებთ)
+# num(4)
+# num(11)
+# num(6)
+# num(7)
+# num(5)
+
+
+#          d r a w    a   a p a r t a m e n t
+
+# from turtle import*
+
+# #   start draw apartament -->
+
+# speed(13)
+# width(5)
+
+# def draw_squear():
+#     for i in range(4):
+#         forward(100)
+#         left(90)
+
+# def move_cursor(x, y):
+#     penup()
+#     goto(x, y)
+#     pendown()
+
+# draw_squear()
+# move_cursor(0, 150)
+
+# draw_squear()
+# move_cursor(-150, 150)
+
+# draw_squear()
+# move_cursor(-150, 0)
+
+# draw_squear()
+# move_cursor(-150, -150)
+
+# draw_squear()
+# move_cursor(0, -150)
+
+# draw_squear()
+# move_cursor(100, -150)
+
+# color("blue")
+# penup()
+# goto(150, -150)
+# pendown()
+# left(90)
+# forward(450)
+# left(90)
+# forward(350)
+# left(90)
+# forward(500)
+# left(90)
+# forward(350)
+# left(90)
+# forward(50)
+
+# penup()
+# goto(150, 300)
+# pendown()
+# left(70)
+# forward(180)
+# left(38)
+# forward(188)
+
+
+# exitonclick()
+
+
+
+#     test -->
+
+# def sey_hi(user_name):
+#     print(user_name + " gamarjoba")
+
+# sey_hi("tazo")
+# sey_hi("temo")
+# sey_hi("nika")
+# sey_hi("luka")
+
+
+# sey_hi(input(sey_hi))
+# sey_hi(input(sey_hi))
+# sey_hi(input(sey_hi))
+# sey_hi(input(sey_hi))
